6f1d22575d0e65d39b3e5a829e7d8e694863f0ff
lehajuk <asitikovdo@mail.com>
2022-06-21 15:07:45 +0900

    add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function start() {
   // const app = await NestFactory.create(AppModule);
   // await app.listen(3000);
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT, () =>
     console.log(`Сервер запустился на порту ${PORT}`),
   );
--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,5 +1,10 @@
+import { IsInt, IsString } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString()
   readonly title: string;
+  @IsString()
   readonly description: string;
+  @IsInt()
   readonly boardId: number;
 }
